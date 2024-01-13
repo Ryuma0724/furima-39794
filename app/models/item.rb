275a016134712db9b3eb5827b310775e002f7a93
Ryuma0724 <s21482804@nucba.ac.jp>
2024-01-13 11:33:37 +0900

    2nd commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
-  belongs_to : user
-  has_many : orders
-  belongs_to : address
+  belongs_to :user
+  has_many :orders
+  belongs_to :address
+
+  validates :text, presence: true
 end
