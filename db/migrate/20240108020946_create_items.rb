5a08bd24b65aad1e07ff11191aed87fed1558292
Ryuma0724 <s21482804@nucba.ac.jp>
2024-01-21 14:58:26 +0900

    13th commit

--- db/migrate/20240108020946_create_items.rb
+++ db/migrate/20240108020946_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
       t.string  :title,  null: false
-      t.string  :content,  null: false
+      t.text  :content,  null: false
       t.integer  :genre_id,  null: false
       t.integer  :condition_id,  null: false
       t.integer  :charge_id,  null: false
