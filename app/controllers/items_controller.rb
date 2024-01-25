a3631a3c8bd3ef7244068a21cc527efd2ac1f748
Ryuma0724 <s21482804@nucba.ac.jp>
2024-01-25 17:01:31 +0900

    5th commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  befroe_action :item_find, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,20 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user.id != @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params) 
-       redirect_to root_path
+       redirect_to item_path
     else
        render :edit, status: :unprocessable_entity
     end
@@ -41,6 +39,10 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :title, :content, :genre_id, :condition_id, :charge_id, :area_id, :leadtime_id, :price).merge(user_id: current_user.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
   end
   
 end
