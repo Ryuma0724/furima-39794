4ff944f984c7113e8eabee22d7058f72e608dba6
Ryuma0724 <s21482804@nucba.ac.jp>
2024-01-25 18:35:20 +0900

    4th commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,9 +38,9 @@ class ItemsController < ApplicationController
 
   def destroy
     if current_user.id = @item.user_id
-      @item.destroy
-      redirect_to root_path
+       @item.destroy
     end
+    redirect_to root_path
   end
 
   private
