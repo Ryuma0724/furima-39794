dd759c39b1eb8297e25ff00588e1920415512084
Ryuma0724 <s21482804@nucba.ac.jp>
2024-01-27 19:04:47 +0900

    4th commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@ class OrdersController < ApplicationController
   
   def index
     @item = Item.find(params[:item_id])
+    @address_order = AddressOrder.new
   end
 
   def new
@@ -10,18 +11,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @address_order = AddressOrder.new(address_params)
     if @address_order.valid?
       @address_order.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
 
   def address_params
-    params.require(:address_order).permit(:zip_code, :area_id, :municipality, :street, :building, :phone_number, :order_id, :user_id, :item_id).merge(user_id: current_user.id)
+    params.require(:address_order).permit(:zip_code, :area_id, :municipality, :street, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end  
 end
--- app/models/address_order.rb
+++ app/models/address_order.rb
@@ -3,15 +3,17 @@ class AddressOrder
   attr_accessor :zip_code, :area_id, :municipality, :street, :building, :phone_number, :order_id, :user_id, :item_id
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
     validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :street
+    validates :phone_number
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :area_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :municipality, presence: true
 
   def save
-    order = Order.create(price: price, user_id: user_id)
-    Address.create(postal_code: zip_code, prefecture: area_id, city: street, building_name: building, phone_number: phone_number, order_id: order_id)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(zip_code: zip_code, area_id: area_id, municipality: municipality, street: street, building: building, phone_number: phone_number, order_id: order.id)
   end
 
 end
