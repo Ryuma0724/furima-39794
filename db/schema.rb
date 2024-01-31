a9502f38563da9dd6dafda23b74487abeae12541
Ryuma0724 <s21482804@nucba.ac.jp>
2024-01-31 17:41:40 +0900

    12th commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_27_035541) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_31_084023) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -93,7 +93,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_27_035541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
-    t.string "name"
     t.string "family_name"
     t.string "given_name"
     t.string "family_name_kana"
