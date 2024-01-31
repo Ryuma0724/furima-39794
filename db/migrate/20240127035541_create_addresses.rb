26828ecfcb2323e070976a0adebf8fb46b3197f9
Ryuma0724 <s21482804@nucba.ac.jp>
2024-01-31 14:38:52 +0900

    11th commit

--- db/migrate/20240127035541_create_addresses.rb
+++ db/migrate/20240127035541_create_addresses.rb
@@ -5,7 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[7.0]
       t.integer    :area_id,    null: false
       t.string     :municipality,    null: false
       t.string     :street,    null: false
-      t.string     :building,    
+      t.string     :building    
       t.string     :phone_number,    null: false
       t.references :order,    null: false, foreign_key: true
       t.timestamps
